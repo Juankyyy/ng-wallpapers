010e55b35f3e231f7cfa79ea73ce6bfac7d5ff25
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-09-11 00:55:54 -0500

    feat: Hero

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,7 +6,24 @@ import { CardWallpaperComponent } from '../../home/card-wallpaper/card-wallpaper
   standalone: true,
   imports: [CardWallpaperComponent],
   templateUrl: './home-page.component.html',
-  styles: ``
+  styles: `
+    @keyframes scroll {
+      0% {
+        transform: translateY(0);
+      }
+      30% {
+        transform: translateY(100px);
+      }
+    }
+
+    svg #wheel {
+      animation: scroll ease 1.5s infinite;
+    }
+
+    section {
+      height: calc(100dvh - 75px);
+    }
+  `
 })
 export class HomePageComponent {
 
