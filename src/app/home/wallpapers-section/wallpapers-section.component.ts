31e3794b89a585fdcf7deaf97ef6b82cde02b356
Juankyyy <103221572+Juankyyy@users.noreply.github.com>
2024-09-13 03:50:53 -0500

    feat: Consumo de api para imagenes
    Service, interface, HttpClient

--- src/app/home/wallpapers-section/wallpapers-section.component.ts
+++ src/app/home/wallpapers-section/wallpapers-section.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { OnInit } from '@angular/core';
 import { CardWallpaperComponent } from '../../home/card-wallpaper/card-wallpaper.component';
+import { WallpapersService } from '../../services/wallpapers.service';
+import { WallpaperResults } from '../../interfaces/wallpapers';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'WallpapersSection',
   standalone: true,
-  imports: [CardWallpaperComponent, NgFor],
+  imports: [CardWallpaperComponent, AsyncPipe],
   templateUrl: './wallpapers-section.component.html',
   styleUrl: './wallpapers-section.component.css'
 })
-export class WallpapersSectionComponent {
-  items = new Array(10);
+export class WallpapersSectionComponent implements OnInit {
+  public wallpaperResults$!: Observable<WallpaperResults>;
+  constructor(private service: WallpapersService) { }
+
+  ngOnInit(): void {
+    this.wallpaperResults$ = this.service.getWallpapers();
+  }
 }
